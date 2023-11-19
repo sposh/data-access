02f1caa45aa03cf930daf6ab1b052afb25842662
Joshua Hewitt <sposh@sposh.net>
2023-11-19 19:04:37 +0100

    feat(*): change comments

--- __tests__/FetchJsonDao.js
+++ __tests__/FetchJsonDao.js
@@ -2,17 +2,14 @@ import { FetchJsonDao } from '..';
 // import fetch from 'node-fetch';
 
 // jest.mock('fetch'); // FIXME Use mock responses - throwing "require is not defined" error
-// const mockId = id => fetch.mockResolvedValue(Promise.resolve({ json: () => Promise.resolve([{ id: id }]) }));
 
 test('Basic FetchJsonDao', async () => {
-    // mockId(696815904);
     const dao1 = new FetchJsonDao();
     dao1.read('https://api.github.com/users/sposh/repos');
     expect((await dao1.dataStream.current)._params[0][0].id).toBe(696815904);
     const dao2 = new FetchJsonDao();
     dao2.read('https://api.github.com/users/sposh/repos');
     expect((await dao2.dataStream.current)._params[0][0].id).toBe(696815904);
-    // mockId(112634155);
     dao1.read('https://api.github.com/users/jane/repos');
     expect((await dao1.dataStream.current)._params[0][0].id).toBe(112634155);
 });
